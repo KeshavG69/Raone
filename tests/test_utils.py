134d87bb2f4507b96d6f6d3300003d5ae1d952ae
Keshav Garg <98670495+KeshavG69@users.noreply.github.com>
2025-06-22 04:26:37 +0530

    Delete tests directory

--- tests/test_utils.py
+++
@@ -1,6 +0,0 @@
-# example tests for utils.py
-import pytest
-
-def test_time_formats():
-    result = utils.format_time(11, 30)
-    assert result == "11:30"
--- tests/test_whatsapp_response.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-def test_arg(response_args):
-    result = whatsqap_response.arg(response_args)
-    assert attype(result)
